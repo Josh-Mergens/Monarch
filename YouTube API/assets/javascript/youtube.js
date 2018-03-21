08469a7c3927dae73ee1bc192b906c1dd14b65e2
Roarke Catubig <roarkecatubig@gmail.com>
2018-03-20 20:28:51 -0700

    Progress update

--- YouTube API/assets/javascript/youtube.js
+++ YouTube API/assets/javascript/youtube.js
@@ -116,7 +116,7 @@ function showNextPage() {
 
     var params = $.param({
         part: 'snippet, id',
-        maxResults: '10',
+        maxResults: '1',
         q: keyword,
         pageToken: token,
         type: 'video',    
@@ -194,7 +194,8 @@ function showVideos(item) {
     '<div class="list-left">' + 
     '<img src="'+thumb+'">' +
     '</div>' +
-    '<div class="list-right">' +
+    '<div class="list-right">' + 
+    // '<iframe id="show-video"></iframe>' +
     '<h3><a class="fancybox fancybox.iframe" href="http://wwww.youtube.com/embed/'+videoId+'">'+title+'</a></h3>' +
     '<small>By <span class="cTitle">'+channelTitle+'</span> on '+publishedAt+'</small>' +
     '<p>'+description+'</p>' + 
